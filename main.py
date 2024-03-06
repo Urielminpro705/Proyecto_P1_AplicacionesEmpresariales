4e46016661ca6df400e7cef7ef0c59c46e929d03
Uriel Mata Castellanos <urielmata37@hotmail.com>
2024-03-06 17:12:52 -0600

    XD

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+
+app = FastAPI()
+app.title = "Cosmobius X Proyecto"
+app.version = "0.0.1"
