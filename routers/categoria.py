e5598afbe1547cf2f599de4b3b39c608c53319f0
Uriel Mata <urielmata37@hotmail.com>
2024-05-05 14:46:27 -0600

    Co-authored-by: livnitro <livnitro@users.noreply.github.com> Co-authored-by: Haziel Deviano <HazielDev@users.noreply.github.com>UWU

--- routers/categoria.py
+++ routers/categoria.py
@@ -1,8 +1,12 @@
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
-from fastapi import APIRouter, Body, Path, Query
-
+from fastapi import APIRouter, Body, Depends, Path, Query
+from middlewares.jwt_bearer import JWTBearer
+from models.categoria import Categoria as CategoriaModel
+from models.libro import Libro as LibroModel
+from config.database import Session
 
 categoria_router = APIRouter()
 
@@ -11,7 +15,6 @@ categorias = []
 class Categoria(BaseModel):
     id: Optional[int] = None
     nombre: str = Field(min_length=1, max_length=30)
-    libros: Optional[int] = 0
 
     class Config:
         json_schema_extra = {
@@ -21,59 +24,51 @@ class Categoria(BaseModel):
         }
 
 # Mostrar todas las categorias
-@categoria_router.get('/categorias', tags=['categorias'], response_model = List[dict], status_code = 200)
-def get_categorias() -> List[dict]:
-    return JSONResponse(status_code = 200, content=categorias)
+@categoria_router.get('/categorias', tags=['categorias'], response_model = List[Categoria], status_code = 200, dependencies=[Depends(JWTBearer())])
+def get_categorias() -> List[Categoria]:
+    db = Session()
+    result = db.query(CategoriaModel).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 # Buscar categoria por id
-@categoria_router.get('/categorias/{id}', tags=['categorias'], response_model = dict, status_code = 200)
-def get_categoria_by_id(id: int = Path(ge = 1, le = 1000)) -> dict:
-    for item in categorias:
-        if item['id'] == id :
-            return JSONResponse(status_code = 200, content = item)
-    return JSONResponse(status_code = 404, content = {"message": "No se encontro la categoria"})
+@categoria_router.get('/categorias/{id}', tags=['categorias'], response_model = Categoria, status_code = 200, dependencies=[Depends(JWTBearer)])
+def get_categoria_by_id(id: int = Path(ge = 0)) -> Categoria:
+    db = Session()
+    result = db.query(CategoriaModel).filter(CategoriaModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code = 404, content={'message': "Categoria con el id:"+id+" No encontrada :c"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
-# Crear categoria
 @categoria_router.post('/categorias/', tags=['categorias'], response_model = dict, status_code = 200)
-def create_categoria(nombre: str = Query(min_length = 3, max_length = 30)) -> dict:
-    
-    if len(categorias) == 0:
-        id = 1
-    else:
-        ultimo = categorias[-1]
-        id = ultimo["id"] + 1
-    for item in categorias :
-        if item["id"] == nombre:
-            return JSONResponse(content= {"message":"La categoria ya existe"})
-    categorias.append({
-        "id": id,
-        "nombre": nombre,
-        "libros": 0
-    })
-    return JSONResponse(status_code = 200, content= {"message":"La categoria se creo correctamente con id " + str(id)})
+def create_categoria(categoria:Categoria) -> dict:
+    db = Session()
+    new_categoria = CategoriaModel(nombre=categoria.nombre)
+    db.add(new_categoria)
+    db.commit()
+    return JSONResponse(status_code= 200, content={"message": "La categoría: "+ categoria.nombre +" se registro con exito."})
 
-# Borrar categoria (Hecho por el dios del MEWING)
+# Borrar categoria (Hecho por el dios del MEWING HAZIELDEV))
 @categoria_router.delete('/categorias/{id}', tags=['categorias'], response_model= dict, status_code=200)
 def delete_cat(id:int = Path(ge = 1, le = 1000)) -> dict:
-    name = ""
-    for cat in categorias:
-        if(cat["id"] == id):
-            if(cat["libros"] == 0):
-                name = cat['nombre']
-                categorias.remove(cat)
-                return JSONResponse(status_code=200, content={"message": "La categoria: "+name+" se borro con exito 👍"})
-            else:
-                return JSONResponse(status_code=409, content={"message":"No se puede borrar una categoria que tenga libros dentro de ella"})
-    return JSONResponse(status_code=404, content={"message": "No se encontro la categoria con el id: "+str(id)})
-        
+    db = Session()
+    result = db.query(CategoriaModel).filter(CategoriaModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code= 404, content = {"message":"No se encontro la categoria con el id: ("+id+") :c"})
+    libros = db.query(LibroModel).filter(LibroModel.categoriaId == id).all()
+    if not libros:
+        librosCount = len(libros)
+        return JSONResponse(status_code=404, content= {"message":"No se puede borrar porque hay ("+librosCount+") libros en esta categoria"})
+    db.delete()
+    db.commit()
+    return JSONResponse(status_code= 200, content={"message":"Se borro la categoria ("+result.nombre+")"})
+    
 # Actualizar categoria
 @categoria_router.put('/categorias/{id}', tags=['categorias'], response_model = dict, status_code = 200)
-def update_categoria(id: int = Path(ge = 1, le = 1000) , nombre: str = Body()) -> dict:
-    for item in categorias:
-        if item["id"] == id:
-            for libro in libros:
-                if libro["categoria"] == item["nombre"]:
-                    libro["categoria"] = nombre
-            item["nombre"] = nombre
-            return JSONResponse(status_code = 200 ,content = {"message": "Se actualizo correctamente la categoria: "+nombre+" 👍"})
-    return JSONResponse(status_code = 404, content = {"mesage": "La categoria: "+nombre+" no existe 😔"})
+def update_categoria(id: int, categoria: Categoria) -> dict:
+    db = Session()
+    result = db.query(CategoriaModel).filter(CategoriaModel.id==id).first()
+    if not result:
+        return JSONResponse(status_code = 404, content = {"message": "No se encontró la categoría."})
+    result.nombre = categoria.nombre
+    db.commit()
+    return JSONResponse(status_code=200, content = {"message": "Se actualizó correctamente la categoría."})
--- routers/libro.py
+++ routers/libro.py
@@ -17,12 +17,11 @@ class Libro(BaseModel):
     autor: str  = Field(min_length = 1, max_length = 30)
     año: int = Field(le=2024)
     categoria: str = Field(min_length = 3, max_length = 20)
-    numPaginas: int = Field(le = 3, max_length = 2500)
+    numPaginas: int = Field(ge = 1, le = 3000)
 
     class Config:
         json_schema_extra = {
             "example": {
-                "codigo": 1,
                 "titulo": "Mi libro",
                 "autor":"Pepe",
                 "año": 2024,
@@ -44,73 +43,56 @@ def get_libro(id: int = Path(ge = 1)) -> Libro:
     db = Session()
     result = db.query(LibroModel).filter(LibroModel.id == id).first()
     if not result:
-        return JSONResponse(status_code = 404, content={'message': "No encontrado"})
+        return JSONResponse(status_code = 404, content={'message': "Libro no encontrado"})
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 # Buscar libros por categoria (Revisado por el God del Mewing)
-@libro_router.get('/libros/', tags=['libros'], response_model = List[dict], status_code = 200, dependencies=[Depends(JWTBearer())])
-def get_libros_by_categoria(categoria: str = Query(min_length = 3, max_length = 30)) -> List[dict]:
+@libro_router.get('/libros/', tags=['libros'], response_model = List[Libro], status_code = 200, dependencies=[Depends(JWTBearer())])
+def get_libros_by_categoria(categoria: str = Query(min_length = 3, max_length = 30)) -> List[Libro]:
     db = Session()
     result = db.query(LibroModel).join(LibroModel.categoria).filter(CategoriaModel.nombre == categoria).all()
     return JSONResponse(status_code = 200, content=jsonable_encoder(result))
 
+# Agregar un libro
+# (TAMBIEN HECHO POR EL DIOS DEL MEWING)  revivan la grasa :v (livliv)   
+@libro_router.post('/libros/', tags=['libros'], response_model= dict, status_code=200, dependencies=[Depends(JWTBearer())])
+def create_libro(libro: Libro)->dict:
+    db = Session()
+    # Verificar si la categoría existe
+    categoria = db.query(CategoriaModel).filter(CategoriaModel.nombre == libro.categoria).first()
+    if not categoria:
+        return JSONResponse(status_code = 404, content={"message": "La categoría no existe."})
+    new_libro = LibroModel(**libro.model_dump(), categoriaId = categoria.id)
+    db.add(new_libro)
+    db.commit()
+    return JSONResponse(status_code= 200, content={"message": "El libro: "+ libro.titulo +" se registro con exito!"})
+
 # Eliminar un libro
-# HECHO POR HAZIEL DEV (EL DIOS DEL MEWING):
-@libro_router.delete('/libros/{codigo}', tags=['libros'], response_model = dict,  status_code= 200, dependencies=[Depends(JWTBearer())])
-def delete_libro(codigo: int = Path(ge = 1, le = 1000)) -> dict:
-    nombre = ""
-    for item in libros:
-        if item["codigo"] == codigo:
-            nombre = item['titulo']
-            libros.remove(item)
-            for cat in categorias:
-                if cat["nombre"] == item["categoria"]:
-                    cat["libros"] -= 1 
-            return JSONResponse(status_code= 200, content= {"message": "El libro: "+nombre+" se borro con exito"})
-    return JSONResponse(status_code= 404, content= {"message": "no se encontro el id: "+ str(codigo)})
+# HECHO POR HAZIEL DEV (EL DIOS DEL MEWING)
+# Editado por livers (skibidi mewing sigma digital circus chamba fortnite)
+@libro_router.delete('/libros/{codigo}', tags=['libros'], response_model = dict, status_code= 200, dependencies=[Depends(JWTBearer())])
+def delete_libro(id: int = Path(ge = 0)) -> dict:
+    db = Session()
+    result = db.query(LibroModel).filter(LibroModel.id==id).first()
+    if not result:
+        return JSONResponse(status_code= 404, content= {"message": "no se encontro el id: "+ str(id)})
+    titulo = result.titulo
+    db.delete(result)
+    db.commit()
+    return JSONResponse(status_code= 200, content= {"message": "El libro: " + titulo + " se borró con exito"})
             
 # Actualizar un libro
+# livliv (¿eeeeeeees confuso verdad? sin embargo, skibidi mewing sigma está mal, todo el globo de texto te lo hace saber. te notas chad, con pensamientos en decadencia. un sentimiento de que el prime no volverá a ser lo mismo.)
 @libro_router.put('/libros/{codigo}', tags=['libros'], response_model = dict, status_code = 200, dependencies=[Depends(JWTBearer())])
-def update_libro(codigo: int = Path(ge = 1, le = 1000), titulo: str=Body(), autor: str=Body(), año: int=Body(), categoria: str=Body(), numPaginas: int=Body()):
-    for item in categorias:
-        if item["nombre"] == categoria:
-            for item in libros:
-                if item["codigo"] == codigo:
-                    item["titulo"] = titulo
-                    item["autor"] = autor
-                    item["año"] = año
-                    item["categoria"] = categoria
-                    item["numPaginas"] = numPaginas
-                    return JSONResponse(status_code=200, content = {"message": "Se actualizó correctamente el libro."})
-            return JSONResponse(status_code = 404, content = {"message": "No se encontró el libro."})
-    return JSONResponse(status_code = 404, content={"message": "La categoría especificada no existe."})
-
-
-# Agregar un libro
-# (TAMBIEN HECHO POR EL DIOS DEL MEWING)       
-@libro_router.post('/libros/', tags=['libros'], response_model= dict, status_code=200, dependencies=[Depends(JWTBearer())])
-def create_libro(nombre: str = Body(), autor: str = Body(), año: int = Body(), categoria: str = Body(), numPaginas: int = Body())->dict:
-    exists = False
-    #//? Aqui empieza el ciclo for, donde se verifica que exista la categoria y asignar el id
-    for cat in categorias:
-        if cat['nombre'] == categoria:
-            exists = True
-            cat["libros"] += 1 
-            if len(libros) > 0:
-                ultimo = libros[-1]
-                cod = ultimo["codigo"]
-            else:
-                cod = 0     
-    #//? Aqui termina el ciclo for
-    if exists == True:
-        libros.append({
-            "codigo": (cod + 1),
-            "nombre": nombre,
-            "autor": autor,
-            "año": año,
-            "categoria": categoria,
-            "numPaginas": numPaginas
-        })
-        return JSONResponse(status_code= 200, content={"message": "El libro: "+nombre+" se registro con exito! con id " + str(cod + 1)})
-    else:
-        return JSONResponse(status_code= 409, content = {"message": "No se encontro la categoria: "+categoria})
+def update_libro(id: int, libro:Libro) -> dict:
+    db = Session()
+    result = db.query(LibroModel).filter(LibroModel.id==id).first()
+    if not result:
+        return JSONResponse(status_code = 404, content = {"message": "No se encontró el libro."})
+    result.titulo = libro.titulo
+    result.autor = libro.autor
+    result.año = libro.año
+    result.categoria = libro.categoria
+    result.numPaginas = libro.numPaginas
+    db.commit()
+    return JSONResponse(status_code=200, content = {"message": "Se actualizó correctamente el libro."})
