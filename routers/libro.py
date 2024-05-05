4fae1336876fdc84d5f0c263ff018cd60cebf075
Uriel Mata <urielmata37@hotmail.com>
2024-05-05 11:41:31 -0600

    XD

--- routers/libro.py
+++ routers/libro.py
@@ -54,63 +54,63 @@ def get_libros_by_categoria(categoria: str = Query(min_length = 3, max_length = 
     result = db.query(LibroModel).join(LibroModel.categoria).filter(CategoriaModel.nombre == categoria).all()
     return JSONResponse(status_code = 200, content=jsonable_encoder(result))
 
-# # Eliminar un libro
-# # HECHO POR HAZIEL DEV (EL DIOS DEL MEWING):
-# @libro_router.delete('/libros/{codigo}', tags=['libros'], response_model = dict,  status_code= 200, dependencies=[Depends(JWTBearer())])
-# def delete_libro(codigo: int = Path(ge = 1, le = 1000)) -> dict:
-#     nombre = ""
-#     for item in libros:
-#         if item["codigo"] == codigo:
-#             nombre = item['titulo']
-#             libros.remove(item)
-#             for cat in categorias:
-#                 if cat["nombre"] == item["categoria"]:
-#                     cat["libros"] -= 1 
-#             return JSONResponse(status_code= 200, content= {"message": "El libro: "+nombre+" se borro con exito"})
-#     return JSONResponse(status_code= 404, content= {"message": "no se encontro el id: "+ str(codigo)})
+# Eliminar un libro
+# HECHO POR HAZIEL DEV (EL DIOS DEL MEWING):
+@libro_router.delete('/libros/{codigo}', tags=['libros'], response_model = dict,  status_code= 200, dependencies=[Depends(JWTBearer())])
+def delete_libro(codigo: int = Path(ge = 1, le = 1000)) -> dict:
+    nombre = ""
+    for item in libros:
+        if item["codigo"] == codigo:
+            nombre = item['titulo']
+            libros.remove(item)
+            for cat in categorias:
+                if cat["nombre"] == item["categoria"]:
+                    cat["libros"] -= 1 
+            return JSONResponse(status_code= 200, content= {"message": "El libro: "+nombre+" se borro con exito"})
+    return JSONResponse(status_code= 404, content= {"message": "no se encontro el id: "+ str(codigo)})
             
 # Actualizar un libro
-# @libro_router.put('/libros/{codigo}', tags=['libros'], response_model = dict, status_code = 200, dependencies=[Depends(JWTBearer())])
-# def update_libro(codigo: int = Path(ge = 1, le = 1000), titulo: str=Body(), autor: str=Body(), año: int=Body(), categoria: str=Body(), numPaginas: int=Body()):
-#     for item in categorias:
-#         if item["nombre"] == categoria:
-#             for item in libros:
-#                 if item["codigo"] == codigo:
-#                     item["titulo"] = titulo
-#                     item["autor"] = autor
-#                     item["año"] = año
-#                     item["categoria"] = categoria
-#                     item["numPaginas"] = numPaginas
-#                     return JSONResponse(status_code=200, content = {"message": "Se actualizó correctamente el libro."})
-#             return JSONResponse(status_code = 404, content = {"message": "No se encontró el libro."})
-#     return JSONResponse(status_code = 404, content={"message": "La categoría especificada no existe."})
+@libro_router.put('/libros/{codigo}', tags=['libros'], response_model = dict, status_code = 200, dependencies=[Depends(JWTBearer())])
+def update_libro(codigo: int = Path(ge = 1, le = 1000), titulo: str=Body(), autor: str=Body(), año: int=Body(), categoria: str=Body(), numPaginas: int=Body()):
+    for item in categorias:
+        if item["nombre"] == categoria:
+            for item in libros:
+                if item["codigo"] == codigo:
+                    item["titulo"] = titulo
+                    item["autor"] = autor
+                    item["año"] = año
+                    item["categoria"] = categoria
+                    item["numPaginas"] = numPaginas
+                    return JSONResponse(status_code=200, content = {"message": "Se actualizó correctamente el libro."})
+            return JSONResponse(status_code = 404, content = {"message": "No se encontró el libro."})
+    return JSONResponse(status_code = 404, content={"message": "La categoría especificada no existe."})
 
 
-# # Agregar un libro
-# # (TAMBIEN HECHO POR EL DIOS DEL MEWING)       
-# @libro_router.post('/libros/', tags=['libros'], response_model= dict, status_code=200, dependencies=[Depends(JWTBearer())])
-# def create_libro(nombre: str = Body(), autor: str = Body(), año: int = Body(), categoria: str = Body(), numPaginas: int = Body())->dict:
-#     exists = False
-#     #//? Aqui empieza el ciclo for, donde se verifica que exista la categoria y asignar el id
-#     for cat in categorias:
-#         if cat['nombre'] == categoria:
-#             exists = True
-#             cat["libros"] += 1 
-#             if len(libros) > 0:
-#                 ultimo = libros[-1]
-#                 cod = ultimo["codigo"]
-#             else:
-#                 cod = 0     
-#     #//? Aqui termina el ciclo for
-#     if exists == True:
-#         libros.append({
-#             "codigo": (cod + 1),
-#             "nombre": nombre,
-#             "autor": autor,
-#             "año": año,
-#             "categoria": categoria,
-#             "numPaginas": numPaginas
-#         })
-#         return JSONResponse(status_code= 200, content={"message": "El libro: "+nombre+" se registro con exito! con id " + str(cod + 1)})
-#     else:
-#         return JSONResponse(status_code= 409, content = {"message": "No se encontro la categoria: "+categoria})
+# Agregar un libro
+# (TAMBIEN HECHO POR EL DIOS DEL MEWING)       
+@libro_router.post('/libros/', tags=['libros'], response_model= dict, status_code=200, dependencies=[Depends(JWTBearer())])
+def create_libro(nombre: str = Body(), autor: str = Body(), año: int = Body(), categoria: str = Body(), numPaginas: int = Body())->dict:
+    exists = False
+    #//? Aqui empieza el ciclo for, donde se verifica que exista la categoria y asignar el id
+    for cat in categorias:
+        if cat['nombre'] == categoria:
+            exists = True
+            cat["libros"] += 1 
+            if len(libros) > 0:
+                ultimo = libros[-1]
+                cod = ultimo["codigo"]
+            else:
+                cod = 0     
+    #//? Aqui termina el ciclo for
+    if exists == True:
+        libros.append({
+            "codigo": (cod + 1),
+            "nombre": nombre,
+            "autor": autor,
+            "año": año,
+            "categoria": categoria,
+            "numPaginas": numPaginas
+        })
+        return JSONResponse(status_code= 200, content={"message": "El libro: "+nombre+" se registro con exito! con id " + str(cod + 1)})
+    else:
+        return JSONResponse(status_code= 409, content = {"message": "No se encontro la categoria: "+categoria})
